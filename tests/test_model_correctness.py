652e3c41c83f019a4aa1aacbeb1f3ab616e3ceea
hoangtv2000 <hoangtv21082000@gmail.com>
2024-03-05 20:56:15 +0700

    format by black

--- tests/test_model_correctness.py
+++ tests/test_model_correctness.py
@@ -4,6 +4,7 @@ import pandas as pd
 # Define path to our model
 MODEL_DIR = "models"
 
+
 def test_model_correctness():
     clf = joblib.load(f"{MODEL_DIR}/model.pkl")
     data = {
@@ -17,6 +18,6 @@ def test_model_correctness():
         "YearRemodAdd": 1978,
         "Exterior1st": "HdBoard",
         "BsmtFinSF2": 0,
-        "TotalBsmtSF": 672
+        "TotalBsmtSF": 672,
     }
     assert clf.predict(pd.DataFrame(data, index=[0]))[0] == 157551.3761237591
